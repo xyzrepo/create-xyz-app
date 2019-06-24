7c2a4757e28e74d44d75c038509e2e8fe426081c
Baker <root@baker.io>
2019-06-24 05:33:22 +0300

    Added Semantic-UI to Frameworks

---
+++ template/frameworks/semantic-ui/plugins/semantic.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import jquery from 'jquery';
+import 'fomantic-ui-css/semantic.min';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.min.css';
+if (process.browser) { 
+    Vue.use(jquery, {name: '$'});
+    Vue.use(VueAwesomeSwiper, {name: 'swiper'});
+}
